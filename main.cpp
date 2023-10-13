3ad3425733a869bf3209f40cbfd48cf3c9ffcc65
Дмитрий Барыкин <dbarykin@astralinux.ru>
2023-10-13 04:18:57 +0300

    lab 3

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void Task1(){
+    cout << "1" << endl;
+}
+
+void Task2(){
+
+}
+
+void Task3(){
+
+}
+
+int main(){
+    cout << "Please enter task number " << endl;
+    int task_number = 0;
+    cin >> task_number;
+    switch (task_number) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+    //    ...
+    }
+
+    return 0;
+}
